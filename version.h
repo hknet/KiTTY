4d31db187d0b195d3af8a79ab085962655720e12
Harald Kapper <4014716+hknet@users.noreply.github.com>
2026-06-17 01:25:54 +0200

    release: bump to 0.84.1.2-beta
    Follow-up to 0.84.1.1-beta carrying the kittygen-cli progname fix
    (59b2468). MSI ProductVersion 0.84.102. README download links corrected
    to the -beta MSI filenames.

--- version.h
+++ version.h
@@ -8,6 +8,6 @@
  * default stuff used for local development runs of 'make'.
  */
 
-#define TEXTVER "0.84.1.1 (KiTTY, PuTTY 0.84 base)"
+#define TEXTVER "0.84.1.2 (KiTTY, PuTTY 0.84 base)"
 #define SSHVER "_Release_0.84"
-#define BINARY_VERSION 0,84,1,1
+#define BINARY_VERSION 0,84,1,2
