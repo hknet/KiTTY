fc7a6a5b2d10a2df27ca5256476dad494531f5bd
Harald Kapper <4014716+hknet@users.noreply.github.com>
2026-06-16 03:19:50 +0200

    Fix SSH interactive prompts regression (far2l struct fields must be unconditional - ODR/ldisc corruption); config Updates button; About web->GitHub; README far2l credit; 0.84.0.17-beta

--- version.h
+++ version.h
@@ -8,6 +8,6 @@
  * default stuff used for local development runs of 'make'.
  */
 
-#define TEXTVER "0.84.0.16-beta (KiTTY, PuTTY 0.84 base)"
+#define TEXTVER "0.84.0.17-beta (KiTTY, PuTTY 0.84 base)"
 #define SSHVER "_Release_0.84"
-#define BINARY_VERSION 0,84,0,16
+#define BINARY_VERSION 0,84,0,17
