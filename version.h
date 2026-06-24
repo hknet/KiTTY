63f5aec67d5b75f17344294a26c59731ff02c956
Harald Kapper <4014716+hknet@users.noreply.github.com>
2026-06-24 02:51:15 +0200

    release: 0.84.1.31-beta (launcher update balloon + kageant key-use balloon)

--- version.h
+++ version.h
@@ -8,6 +8,6 @@
  * default stuff used for local development runs of 'make'.
  */
 
-#define TEXTVER "0.84.1.30 (KiTTY, PuTTY 0.84 base)"
+#define TEXTVER "0.84.1.31 (KiTTY, PuTTY 0.84 base)"
 #define SSHVER "_Release_0.84"
-#define BINARY_VERSION 0,84,1,30
+#define BINARY_VERSION 0,84,1,31
